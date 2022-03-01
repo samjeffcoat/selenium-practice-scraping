128a49452dc64e5dfa1d158058e6a02757a46c4d
Sam Jeffcoat <44582012+samjeffcoat@users.noreply.github.com>
2022-03-01 03:35:44 +0000

    updated up to deployment to aws lambda

--- scraper.py
+++ scraper.py
@@ -1,3 +1,7 @@
+# import pandas as pd
+import os
+import json
+import smtplib
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -19,6 +23,56 @@ def get_videos(driver):
   videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
   return videos
 
+def parse_videos(video):
+  title_tag = video.find_element(By.ID, 'video-title')
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+
+   
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
+  channel_name = channel_div.text
+
+  description = video.find_element(By.ID, 'description-text').text
+
+  return {
+    'title':title,
+    'url' : url,
+    'thumbnail_url': thumbnail_url,
+    'channel': channel_name,
+    'description' : description
+    
+  }
+
+def send_email(body):
+  SENDER_EMAIL = 'trendingvideos10311982@gmail.com'
+  RECEIVER_EMAIL = 'trendingvideos10311982@gmail.com'
+  SENDER_PASSWORD = os.environ['GMAIL_PASSWORD']
+  try:
+    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server_ssl.ehlo()
+
+    sent_from = SENDER_EMAIL
+    to = [RECEIVER_EMAIL]
+    subject = 'Youtube Trending Videos'
+   
+
+    email_text = f"""
+    From:{SENDER_EMAIL}
+    To: {RECEIVER_EMAIL}
+    Subject: {subject}
+
+    {body}
+
+    """
+    server_ssl.login(SENDER_EMAIL, SENDER_PASSWORD )
+    server_ssl.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, email_text)
+    server_ssl.close()
+  except:
+    print('Something went wrong.....')
+
 if __name__ == "__main__":
   print('Creating driver')
   driver = get_driver()
@@ -27,3 +81,24 @@ if __name__ == "__main__":
   videos = get_videos(driver)
   
   print(f'Found {len(videos)} videos')
+
+
+  print('Parsing the top 10 videos')
+  videos_data = [parse_videos(video) for video in videos[:10]]
+
+  
+  
+
+  #todos number of views and when it was uploaded
+  print('Save the data to a CSV')
+  # videos_df = pd.DataFrame(videos_data)
+  # #print(videos_df)
+  # videos_df.to_csv('trending.csv', index = None)
+  
+  # print(videos_df)
+
+  print('Send the results over email')
+  body = json.dumps(videos_data, indent = 2)
+  send_email(body)
+
+  print('Finished')
